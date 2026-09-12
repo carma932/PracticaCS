7cab01c4b00b075f42fb7e3645ddff1b01121e64
Carlos <carlosmarcar05@gmail.com>
2026-09-11 22:16:17 -0400

    Initial commit

---
+++ src/main/java/com/unifranzhk/practicascasa/PracticascasaApplication.java
@@ -0,0 +1,13 @@
+package com.unifranzhk.practicascasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticascasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticascasaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unifranzhk/practicascasa/PracticascasaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unifranzhk.practicascasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticascasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
